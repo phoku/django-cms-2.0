0e87b6991ef03ce61c0cccdc2df6e74838fbcc49
kblindert <kblindert@trillian.localdomain>
2010-02-16 15:52:48 +0100

    Now the get_plugins function will only generate one queryset if all placeholders have plugins (as was before last change).

--- cms/plugins/utils.py
+++ cms/plugins/utils.py
@@ -4,7 +4,7 @@ from cms.utils import get_language_from_request
 from cms.utils.moderator import get_cmsplugin_queryset
 from cms.conf import settings
 
-def _get_plugins_recursively(request, obj, placeholder, lang):
+def get_plugins_recursively(request, obj, placeholder, lang):
     """
     Get plugins for obj, language and placeholder.
     If no plugins are found and the placeholder config in CMS_PLACEHOLDER_CONF
@@ -13,7 +13,7 @@ def _get_plugins_recursively(request, obj, placeholder, lang):
     with plugins is found or the root page is found.
     """
     if not obj:
-        return None
+        return []
 
     recurse = False
     if settings.CMS_PLACEHOLDER_CONF and placeholder in settings.CMS_PLACEHOLDER_CONF:
@@ -29,13 +29,45 @@ def _get_plugins_recursively(request, obj, placeholder, lang):
     return plugins.order_by('placeholder', 'position').select_related()
 
 def get_plugins(request, obj, placeholder, lang=None):
+    """
+    Get all plugins for a page, placeholder and language.
+
+    Internally this will cache the plugin results for all placeholders,
+    so that ideally only one query will be made for all calls to this.
+    
+    In case a placeholder hasn't any plugins defined on the page
+    get_plugins_recursively is used.
+
+    Returns an iterable with the plugins.
+    """
     if not obj:
         return []
     lang = lang or get_language_from_request(request)
-    cache_key = "_%s_%s_plugins_cache" % (lang, placeholder)
+
+    # STAGE ONE - GET AND CACHE ALL PLACEHOLDERS
+    cache_key = "_%s_plugins_cache" % lang
     if not hasattr(obj, cache_key):
-        setattr(obj, cache_key, _get_plugins_recursively(request, obj, placeholder, lang))
-    return getattr(obj, cache_key)
+        plugins = get_cmsplugin_queryset(request).filter(page=obj, language=lang, parent__isnull=True)
+        plugins = plugins.order_by('placeholder', 'position').select_related()
+        cache_dict = {}
+        for plugin in plugins:
+            if plugin.placeholder not in cache_dict:
+                cache_dict[plugin.placeholder] = []
+            cache_dict[plugin.placeholder].append(plugin)
+        setattr(obj, cache_key, cache_dict)
+    cache_dict = getattr(obj, cache_key)
+
+    # STAGE TWO GET AND CACHE FOR THIS PLACEHOLDER
+    # WITH POSSIBLE RECURSION.
+    placeholder_cache_key = "_%s_placeholder_%s_plugins_cache" % (lang, placeholder)
+    if placeholder in cache_dict:
+        setattr(obj, placeholder_cache_key, cache_dict[placeholder])
+
+    if not hasattr(obj, placeholder_cache_key):
+        # NOTE: We pass obj.parent since we already _know_ that the current page doesn't
+        #       provide the plugins for this placeholder.
+        setattr(obj, placeholder_cache_key, get_plugins_recursively(request, obj.parent, placeholder, lang))
+    return getattr(obj, placeholder_cache_key)
 
 def get_plugin_media(request, plugin):
     instance, plugin = plugin.get_plugin_instance()
